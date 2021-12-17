5658c304a0d751de91eeaf39e0f31bd63f435cda
atanda rasheed <atandarash@gmail.com>
2021-12-17 16:17:52 +0100

    fix: remove tag format assumption

--- lib/fastlane/plugin/semantic_release/actions/analyze_commits.rb
+++ lib/fastlane/plugin/semantic_release/actions/analyze_commits.rb
@@ -73,7 +73,8 @@ module Fastlane
 
         # Tag's format is v2.3.4-5-g7685948
         # See git describe man page for more info
-        tag_name = tag.split('-')[0...-2].join('-').strip
+        # tag_name = tag.split('-')[0...-2].join('-').strip
+        tag_name = tag
         parsed_version = tag_name.match(params[:tag_version_match])
 
         if parsed_version.nil?
--- spec/analyze_commits_spec.rb
+++ spec/analyze_commits_spec.rb
@@ -163,7 +163,7 @@ describe Fastlane::Actions::AnalyzeCommitsAction do
         allow(Fastlane::Actions::AnalyzeCommitsAction).to receive(:get_last_tag).and_return('v1.0.8-1-g71ce4d8')
         allow(Fastlane::Actions::AnalyzeCommitsAction).to receive(:get_commits_from_hash).and_return(commits)
 
-        expect(Fastlane::Actions::AnalyzeCommitsAction).to receive(:get_last_tag_hash).with(tag_name: 'v1.0.8', debug: false)
+        expect(Fastlane::Actions::AnalyzeCommitsAction).to receive(:get_last_tag_hash).with(tag_name: 'v1.0.8-1-g71ce4d8', debug: false)
         expect(execute_lane_test(match: 'v*')).to eq(true)
         expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::RELEASE_NEXT_VERSION]).to eq("1.0.9")
       end
@@ -176,7 +176,7 @@ describe Fastlane::Actions::AnalyzeCommitsAction do
         allow(Fastlane::Actions::AnalyzeCommitsAction).to receive(:get_last_tag).and_return('ios-v1.0.8-beta.1-1-g71ce4d8')
         allow(Fastlane::Actions::AnalyzeCommitsAction).to receive(:get_commits_from_hash).and_return(commits)
 
-        expect(Fastlane::Actions::AnalyzeCommitsAction).to receive(:get_last_tag_hash).with(tag_name: 'ios-v1.0.8-beta.1', debug: false)
+        expect(Fastlane::Actions::AnalyzeCommitsAction).to receive(:get_last_tag_hash).with(tag_name: 'ios-v1.0.8-beta.1-1-g71ce4d8', debug: false)
         expect(execute_lane_test(match: 'v*')).to eq(true)
         expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::RELEASE_NEXT_VERSION]).to eq("1.0.9")
       end
